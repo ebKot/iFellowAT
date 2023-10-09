1968698ff3101bf709beae3dfaf5dbd14caa76be
ebKot <katk6960@gmail.com>
2023-10-09 15:06:33 +0300

    init commit

--- src/test/java/Steps/API1Steps.java
+++ src/test/java/Steps/API1Steps.java
@@ -1,10 +1,9 @@
 package Steps;
 
+import Methods.APIMethods;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
-
-import static io.restassured.RestAssured.given;
 
 public class API1Steps {
     private static final String uri1 = "https://rickandmortyapi.com/api";
@@ -12,22 +11,9 @@ public class API1Steps {
     public static String charId, charName, charLoc, charSpec, charLastEp, lastCharId, lastCharName, lastCharLoc, lastCharSpec;
 
 
-    public Response GETMethod(String uri, String endpoint, int code) {
-        return given()
-                .header("Content-type", "application/json")
-                .baseUri(uri)
-                .when()
-                .get(endpoint)
-                .then()
-                .statusCode(code)
-                .extract()
-                .response();
-    }
-
-
     //    Найти информацию по персонажу Морти Смит.
     public static void gettingCharInfoFunc(String id) {
-        Response gettingCharInfo = new API1Steps().GETMethod(uri1, "/character/" + id, code1);
+        Response gettingCharInfo = new APIMethods().GETMethod(uri1, "/character/" + id, code1);
         charId = new JSONObject(gettingCharInfo.getBody().asString()).get("id").toString();
         charName = new JSONObject(gettingCharInfo.getBody().asString()).get("name").toString();
         charLoc = new JSONObject(gettingCharInfo.getBody().asString()).getJSONObject("location").get("name").toString();
@@ -37,18 +23,18 @@ public class API1Steps {
 
     //    Выбрать из ответа последний эпизод, где появлялся Морти.
     public static void gettingLastEpFunc() {
-        Response gettingLastEp = new API1Steps().GETMethod(uri1, "/character/" + charId, code1);
+        Response gettingLastEp = new APIMethods().GETMethod(uri1, "/character/" + charId, code1);
         int episode = (new JSONObject(gettingLastEp.getBody().asString()).getJSONArray("episode").length() - 1);
         charLastEp = new JSONObject(gettingLastEp.getBody().asString()).getJSONArray("episode").get(episode).toString().replaceAll("[^0-9]", "");
     }
 
     //    Получить из списка последнего эпизода последнего персонажа. Получить данные по местонахождению и расе этого персонажа.
     public static void gettingLastCharFunc() {
-        Response gettingLastCharId = new API1Steps().GETMethod(uri1, "/episode/" + charLastEp, code1);
+        Response gettingLastCharId = new APIMethods().GETMethod(uri1, "/episode/" + charLastEp, code1);
         int character = (new JSONObject(gettingLastCharId.getBody().asString()).getJSONArray("characters").length() - 1);
         lastCharId = new JSONObject(gettingLastCharId.getBody().asString()).getJSONArray("characters").get(character).toString().replaceAll("[^0-9]", "");
 
-        Response gettingLastCharInfo = new API1Steps().GETMethod(uri1, "/character/" + lastCharId, code1);
+        Response gettingLastCharInfo = new APIMethods().GETMethod(uri1, "/character/" + lastCharId, code1);
         lastCharName = new JSONObject(gettingLastCharInfo.getBody().asString()).get("name").toString();
         lastCharLoc = new JSONObject(gettingLastCharInfo.getBody().asString()).getJSONObject("location").get("name").toString();
         lastCharSpec = new JSONObject(gettingLastCharInfo.getBody().asString()).get("species").toString();
--- src/test/java/Steps/API2Steps.java
+++ src/test/java/Steps/API2Steps.java
@@ -1,13 +1,14 @@
 package Steps;
 
+import Methods.APIMethods;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class API2Steps {
@@ -20,18 +21,8 @@ public class API2Steps {
         bodyJson.put("name", "Tomato");
         bodyJson.put("job", "Eat maket");
 
-        given()
-                .header("Content-type", "application/json")
-                .baseUri(uri2)
-                .contentType(ContentType.JSON)
-                .body(bodyJson.toString())
-                .when()
-                .post("/api/users")
-                .then()
-                .log().body()
-                .assertThat()
-                .statusCode(code2)
-                .body(matchesJsonSchemaInClasspath("jsonSchema.json")) // валидируем полученный response через json схему
-                .extract().response();
+        Response createUser = new APIMethods().POSTMethod(uri2, ContentType.JSON, bodyJson.toString(), "/api/users", code2);
+
+        createUser.then().assertThat().body(matchesJsonSchemaInClasspath("jsonSchema.json")); // валидируем полученный response через json схему
     }
 }
